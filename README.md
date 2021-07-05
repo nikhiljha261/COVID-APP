39126b559f9693b79a4347419d8805e0c630e503
NIKHIL JHA <nikhiljha@NIKHILs-MacBook-Air.local>
2021-07-06 00:28:22 +0530

    FINAL PUSH ON GIT HUB.

---
+++ README.md
@@ -0,0 +1,42 @@
+# COVID APP
+		
+
+ABOUT THE PROJECT
+----------------------------
+
+Web app to show the numbers and trends of people getting
+infected , recovered and decreased due to COVID-19.
+The app helps us to spread awareness by visualizing the data
+both Geographically and Graphical 
+
+----------------------------
+
+TECHNOLOGIES USED
+----------------------------
+-  React.JS
+-  Material UI 
+-  Charts.js 
+-  React Hooks
+-  API data fetching (Axios) 
+-  modern JavaScript. 
+
+
+----------------------------
+INSTRUCTIONS TO RUN THE PROJECT
+----------------------------
+
+Type the following commands in sequential order:
+
+ - Install packages
+
+      npm install
+
+ -  Run the development server
+
+      npm start
+              
+
+----------------------------
+
+
+
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./Footer.module.css";
+const Footer = () => {
+  const onMouseOut = (e) => {
+    e.target.style.color = "#cccccc";
+  };
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <h6
+          style={{
+            display: "block",
+            marginBottom: "0px",
+            fontSize: 30,
+            marginTop: "40px",
+            textAlign: "center",
+          }}
+        >
+          Created  by{" "}
+          <a
+            href="https://github.com/nikhiljha261/CORONA-TRACKER"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "rgba(0,0,255,0.6)", textDecoration: "none" }}
+          >
+            Nikhil Jha
+          </a>
+        </h6>
+      </div>
+      <div className={styles.container}>
+        <h6
+          style={{
+            display: "block",
+            marginBottom: "0px",
+            fontSize: 16,
+            marginTop: "20px",
+            textAlign: "center",
+          }}
+        >
+          Data provided  by{" "}
+          <a
+            href="https://github.com/mathdroid/covid-19-api"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "rgba(0,0,255,0.6)", textDecoration: "none" }}
+          >
+            Mathdroid
+          </a>
+        </h6>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
